2a5ea74c65e67753cb23ec4102e20d2b5e3a422e
Matthew Sells <matthew.sells@proton.me>
2025-01-27 16:56:59 -0600

    add base vm parser

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,3 +1,24 @@
+mod parser;
+
+use parser::Parser;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file = File::open(&args[1]).unwrap();
+    let mut parser = Parser::new(&file);
+
+    while parser.has_more_lines() {
+        let current = parser.current();
+
+        println!("{:?}", current);
+
+        parser.advance();
+    }
 }
---
+++ vm/src/parser.rs
@@ -0,0 +1,100 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+pub enum MathOp {
+    Add,
+    Sub,
+}
+
+#[derive(Debug)]
+pub enum StackOp {
+    Push,
+    Pop,
+}
+
+#[derive(Debug)]
+pub enum StackSegment {
+    Constant,
+    Local,
+    Argument,
+    This,
+    That,
+    Temp,
+}
+
+#[derive(Debug)]
+pub enum Command {
+    Arithmetic {
+        op: MathOp,
+    },
+    Stack {
+        op: StackOp,
+        segment: StackSegment,
+        index: u16,
+    },
+}
+
+pub struct Parser {
+    lines: Vec<String>,
+    pos: u16,
+}
+
+impl Parser {
+    pub fn new(file: &File) -> Self {
+        let lines = BufReader::new(file)
+            .lines()
+            .map(|l| l.unwrap().trim().to_string())
+            .filter(|l| !l.starts_with("//") && l.len() > 0)
+            .collect();
+
+        Self { lines, pos: 0 }
+    }
+
+    pub fn has_more_lines(self: &Self) -> bool {
+        self.pos < self.total_lines() - 1
+    }
+
+    pub fn advance(self: &mut Self) {
+        self.pos += 1;
+    }
+
+    pub fn current(self: &Self) -> Command {
+        let string = self.lines[self.pos as usize].clone();
+        let segments: Vec<&str> = string.split_whitespace().collect();
+
+        if segments.len() == 1 {
+            let op = match segments[0] {
+                "add" => MathOp::Add,
+                "sub" => MathOp::Sub,
+                _ => panic!("Invalid arithmetic operation"),
+            };
+
+            Command::Arithmetic { op }
+        } else {
+            let op = match segments[0] {
+                "push" => StackOp::Push,
+                "pop" => StackOp::Pop,
+                _ => panic!("Invalid stack operation"),
+            };
+
+            let segment = match segments[1] {
+                "constant" => StackSegment::Constant,
+                "local" => StackSegment::Local,
+                "argument" => StackSegment::Argument,
+                "this" => StackSegment::This,
+                "that" => StackSegment::That,
+                "temp" => StackSegment::Temp,
+                _ => panic!("Invalid stack segment"),
+            };
+
+            let index = segments[2].parse::<u16>().unwrap();
+
+            Command::Stack { op, segment, index }
+        }
+    }
+
+    fn total_lines(self: &Self) -> u16 {
+        self.lines.len() as u16
+    }
+}
